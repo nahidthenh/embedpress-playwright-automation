c6c874b815e3641ddfd324c221cd3b39c04a32c8
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2024-08-05 10:07:17 +0600

    Fixed Flip Book All Control Enable Widget Failur Test Case

--- tests/gutenberg/flip_book_pdf.spec.js
+++ tests/gutenberg/flip_book_pdf.spec.js
@@ -1,10 +1,9 @@
 const { test, expect } = require('@playwright/test');
 
 let slug = 'flip-book-pdf';
-let heading = 'Gutenberg Modern pdf';
 
 
-test.describe("Gutenberg Modern PDF", () => {
+test.describe("Gutenberg Flip Book PDF", () => {
     test.beforeEach(async ({ page }) => {
         await page.goto(slug);
         await expect(page.getByRole('heading', { name: 'Flip Book Pdf' })).toBeVisible();
@@ -32,10 +31,10 @@ test.describe("Gutenberg Modern PDF", () => {
         await expect(page.frameLocator('#embedpress-pdf-1722764697745 iframe[title="sample_pdf"]').frameLocator('iframe[title="View"]').getByRole('link', { name: 'Sounds' })).toBeVisible();
         await expect(page.getByText('Powered By EmbedPress')).toBeVisible();
         await page.frameLocator('#embedpress-pdf-1722764697745 iframe[title="sample_pdf"]').frameLocator('iframe[title="View"]').getByRole('link', { name: 'Next page' }).first().click();
+        await page.frameLocator('#embedpress-pdf-1722764697745 iframe[title="sample_pdf"]').frameLocator('iframe[title="View"]').getByRole('link', { name: 'Previous page' }).click();
         await page.frameLocator('#embedpress-pdf-1722764697745 iframe[title="sample_pdf"]').frameLocator('iframe[title="View"]').getByRole('link', { name: 'Zoom in' }).click();
         await page.frameLocator('#embedpress-pdf-1722764697745 iframe[title="sample_pdf"]').frameLocator('iframe[title="View"]').getByRole('link', { name: 'Zoom out' }).click();
         await page.frameLocator('#embedpress-pdf-1722764697745 iframe[title="sample_pdf"]').frameLocator('iframe[title="View"]').getByRole('link', { name: 'Table of contents' }).click();
-        await page.frameLocator('#embedpress-pdf-1722764697745 iframe[title="sample_pdf"]').frameLocator('iframe[title="View"]').getByRole('link', { name: 'Previous page' }).click();
         const downloadPromise = page.waitForEvent('download');
         await page.frameLocator('#embedpress-pdf-1722764697745 iframe[title="sample_pdf"]').frameLocator('iframe[title="View"]').getByRole('link', { name: 'Download' }).click();
         const download = await downloadPromise;
@@ -44,8 +43,6 @@ test.describe("Gutenberg Modern PDF", () => {
         await page.frameLocator('#embedpress-pdf-1722764697745 iframe[title="sample_pdf"]').frameLocator('iframe[title="View"]').getByRole('link', { name: 'Single page' }).click();
         await page.frameLocator('#embedpress-pdf-1722764697745 iframe[title="sample_pdf"]').frameLocator('iframe[title="View"]').locator('li').filter({ hasText: 'Smart pan Single page Sounds' }).getByRole('link').click();
         await page.frameLocator('#embedpress-pdf-1722764697745 iframe[title="sample_pdf"]').frameLocator('iframe[title="View"]').getByRole('link', { name: 'Sounds' }).click();
-        await page.frameLocator('#embedpress-pdf-1722764697745 iframe[title="sample_pdf"]').frameLocator('iframe[title="View"]').getByRole('link', { name: 'Next page' }).first().click();
-        await page.frameLocator('#embedpress-pdf-1722764697745 iframe[title="sample_pdf"]').frameLocator('iframe[title="View"]').getByRole('link', { name: 'Previous page' }).click();
     });
 
     test('To Enable Few Controls', async ({ page }) => {
