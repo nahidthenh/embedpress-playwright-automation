a7af31e53aee0ab3a4f89efd0204b506d744a585
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2024-08-13 10:09:16 +0600

    Gutenberg Spotify Single , Playlist , Album , Arist All TestCase

--- tests/gutenberg/spotify.spec.js
+++ tests/gutenberg/spotify.spec.js
@@ -19,13 +19,73 @@ test.describe("Gutenberg Spotify", () => {
         await expect(framelocator.getByRole('link', { name: 'Surah Ar-Rahman (Be Heaven)' })).toBeVisible();
         await expect(framelocator.getByLabel('More')).toBeVisible();
         await expect(framelocator.getByTestId('play-pause-button')).toBeVisible();
-
         await framelocator.getByTestId('play-pause-button').click();
         await framelocator.getByTestId('play-pause-button').click();
-
         await framelocator.getByLabel('Close').click();
+    });
 
+    test('Spotify Artist', async ({ page }) => {
+        const heading = page.getByRole('heading', { name: 'Spotify Artist' });
+        await heading.scrollIntoViewIfNeeded();
+        await expect(heading).toBeVisible();
+        const framelocator = page.frameLocator('iframe[title="Spotify Embed: Omar Hisham"]');
 
+        await expect(framelocator.getByTestId('initialized-true')).toBeVisible();
+        await expect(framelocator.getByTestId('tracklist').locator('div').filter({ hasText: '1Surah Ar-Rahman (Be Heaven)' }).first()).toBeVisible();
+        await expect(framelocator.getByRole('link', { name: 'Omar Hisham' })).toBeVisible();
+        await expect(framelocator.locator('span').filter({ hasText: 'Top tracks' }).first()).toBeVisible();
+        await expect(framelocator.locator('.PlayerControlsShort_playerControlsWrapper__qdkr5')).toBeVisible();
+        await expect(framelocator.getByTestId('play-pause-button')).toBeVisible();
+        await expect(framelocator.getByLabel('More')).toBeVisible();
+        await expect(framelocator.getByTestId('tracklist-row-0')).toBeVisible();
+        await expect(framelocator.getByTestId('tracklist-row-1')).toBeVisible();
+        await expect(framelocator.getByTestId('tracklist-row-2')).toBeVisible();
+    });
+
+    test('Spotify Artist', async ({ page }) => {
+        const heading = page.getByRole('heading', { name: 'Spotify Album' });
+        await heading.scrollIntoViewIfNeeded();
+        await expect(heading).toBeVisible();
+        const framelocator = page.frameLocator('iframe[title="Spotify Embed\\: Al Sabaê Al Mounjiate \\(Quran\\)"]');
+
+        await expect(framelocator).getByTestId('initialized-true').toBeVisible();
+        await expect(framelocator).getByTestId('tracklist').locator('div').filter({ hasText: 'Al FatihaAbd Al Rahman Al' }).nth(1).toBeVisible();
+        await expect(framelocator).getByTestId('tracklist-row-0').toBeVisible();
+        await expect(framelocator).getByTestId('tracklist-row-1').toBeVisible();
+        await expect(framelocator).getByTestId('tracklist-row-2').toBeVisible();
+        await expect(framelocator).getByTestId('play-pause-button').toBeVisible();
+        await expect(framelocator).locator('.PlayerControlsShort_playerControlsWrapper__qdkr5').toBeVisible();
+        await expect(framelocator).getByLabel('More').toBeVisible();
+        await expect(framelocator).getByTestId('spotify-logo').toBeVisible();
+        await expect(framelocator).locator('.CoverArtBase_coverArt__ne0XI').toBeVisible();
+    });
+
+    test('Spotify Playlist', async ({ page }) => {
+        const heading = page.getByRole('heading', { name: 'Spotify Playlist' });
+        await heading.scrollIntoViewIfNeeded();
+        await expect(heading).toBeVisible();
+        const framelocator = page.frameLocator('iframe[title="Spotify Embed\\: Kids Quran playlist"]');
+
+        await expect(framelocator).getByTestId('initialized-true').toBeVisible();
+        await expect(framelocator).getByTestId('tracklist').locator('div').filter({ hasText: '1AlbaqarahMashary Rashid Al-' }).nth(1).toBeVisible();
+        await expect(framelocator).getByTestId('tracklist-row-0').toBeVisible();
+        await expect(framelocator).getByTestId('tracklist-row-1').toBeVisible();
+        await expect(framelocator).getByTestId('tracklist-row-2').toBeVisible();
+        await expect(framelocator).getByTestId('play-pause-button').toBeVisible();
+        await expect(framelocator).getByLabel('More').toBeVisible();
+        await expect(framelocator).locator('.PlayerControlsShort_playerControlsWrapper__qdkr5').toBeVisible();
+        await expect(framelocator).getByTestId('initialized-true').getByTestId('save-on-spotify-button').toBeVisible();
+        await expect(framelocator).getByTestId('spotify-logo').toBeVisible();
+        await expect(framelocator).getByRole('link', { name: 'Kids Quran playlist' }).toBeVisible();
+        await expect(framelocator).locator('.CoverArtBase_coverArt__ne0XI').toBeVisible();
+        await expect(framelocator).getByTestId('play-pause-button').toBeVisible();
+        await framelocator.getByTestId('play-pause-button').click();
+        await framelocator.getByTestId('tracklist-row-0').getByTestId('playback-indicator').click();
+        await framelocator.getByLabel('Close').click();
+        await page.locator('iframe[title="Spotify Embed\\: Kids Quran playlist"]').click();
+        await framelocator.getByRole('heading', { name: 'Listen to these tracks &' }).click();
+        await framelocator.getByLabel('Close').click();
+        await expect(framelocator).getByTestId('tracklist-row-0').getByTestId('playback-indicator').toBeVisible();
     });
 
 });
