2549329111339e2c01dda0dd925712fc7cbe29ec
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2024-08-13 09:49:32 +0600

    Gutenberg Spotify Single TestCase

--- tests/gutenberg/spotify.spec.js
+++ tests/gutenberg/spotify.spec.js
@@ -8,10 +8,24 @@ test.describe("Gutenberg Spotify", () => {
         await page.goto(slug);
     });
 
-    test('Custom Player Preset 1', async ({ page }) => {
-        const heading = page.getByRole('heading', { name: 'Custom Player Preset 1' });
+    test('Spotify Single', async ({ page }) => {
+        const heading = page.getByRole('heading', { name: 'Spotify Single' });
         await heading.scrollIntoViewIfNeeded();
         await expect(heading).toBeVisible();
+        const framelocator = page.frameLocator('iframe[title="Spotify Embed: Surah Ar-Rahman (Be Heaven)"]');
+
+        await expect(framelocator.locator('.BackgroundColorContainer_backgroundColorContainer__YZSQ7')).toBeVisible();
+        await expect(framelocator.locator('.CoverArtBase_coverArt__ne0XI')).toBeVisible();
+        await expect(framelocator.getByRole('link', { name: 'Surah Ar-Rahman (Be Heaven)' })).toBeVisible();
+        await expect(framelocator.getByLabel('More')).toBeVisible();
+        await expect(framelocator.getByTestId('play-pause-button')).toBeVisible();
+
+        await framelocator.getByTestId('play-pause-button').click();
+        await framelocator.getByTestId('play-pause-button').click();
+
+        await framelocator.getByLabel('Close').click();
+
+
     });
 
 });
