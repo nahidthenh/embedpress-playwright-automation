b51c71ef905f41722405a89d7b1db7631f34292b
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2024-08-27 16:52:54 +0600

    YouTube Advanced Layout - Gallery, List, Grid, Carousel TestCase

--- tests/elementor/youtube.spec.js
+++ tests/elementor/youtube.spec.js
@@ -8,6 +8,81 @@ test.describe("Elementor YouTube", () => {
         await page.goto(slug);
     });
 
+    test('YouTube Chanel Gallery', async ({ page }) => {
+        const heading = page.getByRole('heading', { name: 'YouTube Chanel Gallery' });
+        await heading.scrollIntoViewIfNeeded();
+        await expect(heading).toBeVisible();
+        await expect(page.frameLocator('iframe[title="Baseera"]').locator('.ytp-cued-thumbnail-overlay-image')).toBeVisible();
+        await expect(page.locator('.channel-header').first()).toBeVisible();
+        await expect(page.getByRole('heading', { name: 'Baseera' })).toBeVisible();
+        await expect(page.getByRole('img', { name: 'Baseera' })).toBeVisible();
+        await expect(page.locator('#ep-elements-id-7cb3671').getByRole('link', { name: 'Subscribe' })).toBeVisible();
+        await expect(page.locator('.thumb').first()).toBeVisible();
+        await expect(page.locator('.description-container').first()).toBeVisible();
+        await expect(page.locator('div:nth-child(2) > .thumb').first()).toBeVisible();
+        await expect(page.locator('div:nth-child(2) > .body > .description-container').first()).toBeVisible();
+        await expect(page.locator('#ep-elements-id-7cb3671').getByText('1', { exact: true }).first()).toBeVisible();
+        await expect(page.locator('.ep-next').first()).toBeVisible();
+        await page.locator('#ep-elements-id-7cb3671').getByText('Next').click();
+        await page.waitForTimeout(1000)
+        await expect(page.locator('.ep-prev').first()).toBeVisible();
+        await page.locator('.ep-prev').first().click();
+    });
+
+    test('YouTube Chanel List', async ({ page }) => {
+        const heading = page.getByRole('heading', { name: 'YouTube Chanel List' });
+        await heading.scrollIntoViewIfNeeded();
+        await expect(heading).toBeVisible();
+        await expect(page.locator('#b283ee7 > .ose-youtube > .ep-player-wrap > .channel-header')).toBeVisible();
+        await expect(page.getByRole('heading', { name: 'Ahmadullah' })).toBeVisible();
+        await page.getByRole('img', { name: 'Ahmadullah' }).click();
+        await expect(page.locator('#ep-elements-id-b283ee7').getByRole('link', { name: 'Subscribe' })).toBeVisible();
+        await expect(page.locator('div:nth-child(2) > .youtube__content__body > .content__wrap > div > .thumb').first()).toBeVisible();
+        await expect(page.locator('div:nth-child(2) > .youtube__content__body > .content__wrap > div > .body > .description-container').first()).toBeVisible();
+        await expect(page.locator('div:nth-child(2) > .youtube__content__body > .content__wrap > div:nth-child(2)').first()).toBeVisible();
+        await page.locator('div:nth-child(2) > .youtube__content__body > .content__wrap > div > .thumb > .play-icon > img').first().click();
+        await page.waitForTimeout(1000)
+        await expect(page.getByText('×', { exact: true })).toBeVisible();
+        await page.getByText('×', { exact: true }).click();
+    });
+
+    test('YouTube Chanel Grid', async ({ page }) => {
+        const heading = page.getByRole('heading', { name: 'YouTube Chanel List' });
+        await heading.scrollIntoViewIfNeeded();
+        await expect(heading).toBeVisible();
+        await expect(page.locator('[id="\\31 d37408"] > .ose-youtube > .ep-player-wrap > .channel-header')).toBeVisible();
+        await expect(page.getByRole('img', { name: 'Bangladesh Cricket : The' })).toBeVisible();
+        await expect(page.getByRole('heading', { name: 'Bangladesh Cricket : The' })).toBeVisible();
+        await expect(page.locator('#ep-elements-id-1d37408').getByRole('link', { name: 'Subscribe' })).toBeVisible();
+        await expect(page.locator('[id="\\31 d37408"] > .ose-youtube > .ep-player-wrap > .ep-youtube__content__block > .youtube__content__body > .content__wrap > div > .thumb').first()).toBeVisible();
+        await expect(page.locator('[id="\\31 d37408"] > .ose-youtube > .ep-player-wrap > .ep-youtube__content__block > .youtube__content__body > .content__wrap > div:nth-child(2) > .thumb')).toBeVisible();
+        await expect(page.locator('[id="\\31 d37408"] > .ose-youtube > .ep-player-wrap > .ep-youtube__content__block > .youtube__content__body > .content__wrap > div > .body > .description-container').first()).toBeVisible();
+        await expect(page.locator('[id="\\31 d37408"] > .ose-youtube > .ep-player-wrap > .ep-youtube__content__block > .youtube__content__body > .content__wrap > div:nth-child(2) > .body > .description-container')).toBeVisible();
+        await page.locator('[id="\\31 d37408"] > .ose-youtube > .ep-player-wrap > .ep-youtube__content__block > .youtube__content__body > .content__wrap > div:nth-child(2) > .thumb > .play-icon > img').click();
+        await page.waitForTimeout(1000)
+        await expect(page.frameLocator('#videoIframe').locator('video')).toBeVisible();
+        await expect(page.getByText('×', { exact: true })).toBeVisible();
+        await page.getByText('×', { exact: true }).click();
+
+    });
+
+    test('YouTube Chanel Carousel', async ({ page }) => {
+        const heading = page.getByRole('heading', { name: 'YouTube Chanel List' });
+        await heading.scrollIntoViewIfNeeded();
+        await expect(heading).toBeVisible();
+        await expect(page.locator('#bb8a111 > .ose-youtube > .ep-player-wrap > .channel-header')).toBeVisible();
+        await expect(page.getByRole('heading', { name: 'Dhruv Rathee' })).toBeVisible();
+        await expect(page.getByRole('img', { name: 'Dhruv Rathee' })).toBeVisible();
+        await expect(page.locator('#ep-elements-id-bb8a111').getByRole('link', { name: 'Subscribe' })).toBeVisible();
+        await expect(page.locator('#bb8a111 > .ose-youtube > .ep-player-wrap > .ep-youtube__content__block > .youtube__content__body > .content__wrap > div > .thumb').first()).toBeVisible();
+        await expect(page.locator('#bb8a111 > .ose-youtube > .ep-player-wrap > .ep-youtube__content__block > .youtube__content__body > .content__wrap > div > .body > .description-container').first()).toBeVisible();
+        await page.locator('#bb8a111 > .ose-youtube > .ep-player-wrap > .ep-youtube__content__block > .youtube__content__body > .content__wrap > div:nth-child(2) > .thumb > .play-icon > img').click();
+        await page.waitForTimeout(1000)
+        await expect(page.getByText('×', { exact: true })).toBeVisible();
+        await page.getByText('×', { exact: true }).click();
+
+    });
+
     test('Default YouTube', async ({ page }) => {
         const heading = page.getByRole('heading', { name: 'Default YouTube' });
         await heading.scrollIntoViewIfNeeded();
@@ -55,7 +130,7 @@ test.describe("Elementor YouTube", () => {
         const heading = page.getByRole('heading', { name: 'Enable Custom Player and' });
         await heading.scrollIntoViewIfNeeded();
         await expect(heading).toBeVisible();
-        
+
         await expect(page.getByText('Copy URL Form Embed Code')).toBeVisible();
         await expect(page.locator('#ep-elements-id-27221be').getByText('PausePlay')).toBeVisible();
         await expect(page.locator('#ep-elements-id-27221be').getByRole('button', { name: 'Mute' })).toBeVisible();
