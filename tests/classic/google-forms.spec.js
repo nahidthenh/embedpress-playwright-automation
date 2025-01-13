dffb7be74fb775b3eb9020d51f63948488c97f80
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2025-01-13 11:16:40 +0600

    Classic Editor Google Forms

---
+++ tests/classic/google-forms.spec.js
@@ -0,0 +1,24 @@
+const { test, expect } = require('@playwright/test');
+
+const slug = 'playwright-classic-editor/classic-google-forms';
+
+test.describe("Classic Google Forms", () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto(slug);
+        await page.waitForLoadState('networkidle');
+    });
+
+    test('Classic Google Forms', async ({ page }) => {
+        // Check iframe visibility        
+        await expect(page.getByRole('heading', { name: 'Classic Google Forms' })).toBeVisible();
+
+        // Check iframe visibility
+        const iframe = page.locator('iframe').contentFrame().locator('div').filter({ hasText: 'Contact information* প্রয়োজনীয় প্রশ্ন নির্দেশ করেName *আপনার উত্তরEmail' }).first()
+        await expect(iframe).toBeVisible();
+
+        // Check dimensions height & width
+        const { height, width } = await iframe.evaluate(iframe => iframe.getBoundingClientRect());
+        expect(height).toBeCloseTo(1296.703125, 1);
+        expect(width).toBeCloseTo(1140, 1);
+    });
+});
