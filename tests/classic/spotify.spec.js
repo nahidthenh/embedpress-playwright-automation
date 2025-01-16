0d8f2e229f10f911429ce433282122a15a61d1bb
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2025-01-16 09:02:03 +0600

    Store Locator on a variable

--- tests/classic/spotify.spec.js
+++ tests/classic/spotify.spec.js
@@ -13,7 +13,7 @@ test.describe('Classic Spotify', () => {
         await heading.scrollIntoViewIfNeeded();
         await expect(heading).toBeVisible();
 
-        const pageLocator = page.locator('iframe[title="Spotify Embed\\: Kids Quran playlist"]')
+        const pageLocator = page.locator('iframe[title="Spotify Embed\\: Kids Quran playlist"]');
 
         await expect(pageLocator).toBeVisible();
         await expect(pageLocator.contentFrame().locator('.CoverArtBase_coverArt__ne0XI')).toBeVisible();
@@ -32,15 +32,17 @@ test.describe('Classic Spotify', () => {
         await heading.scrollIntoViewIfNeeded();
         await expect(heading).toBeVisible();
 
-        await expect(page.locator('iframe[title="Spotify Embed\\: Surah Ar-Rahman \\(Be Heaven\\)"]').contentFrame().locator('.BackgroundColorContainer_backgroundColorContainer__YZSQ7')).toBeVisible();
-        await expect(page.locator('iframe[title="Spotify Embed\\: Surah Ar-Rahman \\(Be Heaven\\)"]').contentFrame().getByRole('link', { name: 'Surah Ar-Rahman (Be Heaven)' })).toBeVisible();
-        await expect(page.locator('iframe[title="Spotify Embed\\: Surah Ar-Rahman \\(Be Heaven\\)"]').contentFrame().locator('.CoverArtBase_coverArt__ne0XI')).toBeVisible();
-        await expect(page.locator('iframe[title="Spotify Embed\\: Surah Ar-Rahman \\(Be Heaven\\)"]').contentFrame().getByTestId('spotify-logo')).toBeVisible();
-        await expect(page.locator('iframe[title="Spotify Embed\\: Surah Ar-Rahman \\(Be Heaven\\)"]').contentFrame().getByLabel('More')).toBeVisible();
-        await expect(page.locator('iframe[title="Spotify Embed\\: Surah Ar-Rahman \\(Be Heaven\\)"]').contentFrame().getByTestId('play-pause-button')).toBeVisible();
-        await page.locator('iframe[title="Spotify Embed\\: Surah Ar-Rahman \\(Be Heaven\\)"]').contentFrame().getByTestId('play-pause-button').click();
+        const pageLocator = page.locator('iframe[title="Spotify Embed\\: Surah Ar-Rahman \\(Be Heaven\\)"]');
+
+        await expect(pageLocator.contentFrame().locator('.BackgroundColorContainer_backgroundColorContainer__YZSQ7')).toBeVisible();
+        await expect(pageLocator.contentFrame().getByRole('link', { name: 'Surah Ar-Rahman (Be Heaven)' })).toBeVisible();
+        await expect(pageLocator.contentFrame().locator('.CoverArtBase_coverArt__ne0XI')).toBeVisible();
+        await expect(pageLocator.contentFrame().getByTestId('spotify-logo')).toBeVisible();
+        await expect(pageLocator.contentFrame().getByLabel('More')).toBeVisible();
+        await expect(pageLocator.contentFrame().getByTestId('play-pause-button')).toBeVisible();
+        await pageLocator.contentFrame().getByTestId('play-pause-button').click();
         await page.waitForTimeout(300)
-        await page.locator('iframe[title="Spotify Embed\\: Surah Ar-Rahman \\(Be Heaven\\)"]').contentFrame().getByTestId('play-pause-button').click();
+        await pageLocator.contentFrame().getByTestId('play-pause-button').click();
     });
 
     test('Classic Spotify Artist', async ({ page }) => {
@@ -48,15 +50,17 @@ test.describe('Classic Spotify', () => {
         await heading.scrollIntoViewIfNeeded();
         await expect(heading).toBeVisible();
 
-        await expect(page.locator('iframe[title="Spotify Embed\\: Omar Hisham"]').contentFrame().locator('.CoverArtBase_coverArt__ne0XI')).toBeVisible();
-        await expect(page.locator('iframe[title="Spotify Embed\\: Omar Hisham"]').contentFrame().getByTestId('spotify-logo')).toBeVisible();
-        await expect(page.locator('iframe[title="Spotify Embed\\: Omar Hisham"]').contentFrame().getByLabel('More')).toBeVisible();
-        await expect(page.locator('iframe[title="Spotify Embed\\: Omar Hisham"]').contentFrame().getByTestId('play-pause-button')).toBeVisible();
-        await expect(page.locator('iframe[title="Spotify Embed\\: Omar Hisham"]').contentFrame().getByTestId('tracklist-row-0')).toBeVisible();
-        await expect(page.locator('iframe[title="Spotify Embed\\: Omar Hisham"]').contentFrame().getByTestId('tracklist-row-1')).toBeVisible();
-        await page.locator('iframe[title="Spotify Embed\\: Omar Hisham"]').contentFrame().getByTestId('play-pause-button').click();
+        const pageLocator = page.locator('iframe[title="Spotify Embed\\: Omar Hisham"]');
+
+        await expect(pageLocator.contentFrame().locator('.CoverArtBase_coverArt__ne0XI')).toBeVisible();
+        await expect(pageLocator.contentFrame().getByTestId('spotify-logo')).toBeVisible();
+        await expect(pageLocator.contentFrame().getByLabel('More')).toBeVisible();
+        await expect(pageLocator.contentFrame().getByTestId('play-pause-button')).toBeVisible();
+        await expect(pageLocator.contentFrame().getByTestId('tracklist-row-0')).toBeVisible();
+        await expect(pageLocator.contentFrame().getByTestId('tracklist-row-1')).toBeVisible();
+        await pageLocator.contentFrame().getByTestId('play-pause-button').click();
         await page.waitForTimeout(300)
-        await page.locator('iframe[title="Spotify Embed\\: Omar Hisham"]').contentFrame().getByTestId('play-pause-button').click();
+        await pageLocator.contentFrame().getByTestId('play-pause-button').click();
     });
 
 
@@ -65,16 +69,18 @@ test.describe('Classic Spotify', () => {
         await heading.scrollIntoViewIfNeeded();
         await expect(heading).toBeVisible();
 
-       await expect(page.locator('iframe[title="Spotify Embed\\: Al Sabaê Al Mounjiate \\(Quran\\)"]').contentFrame().locator('.CoverArtBase_coverArt__ne0XI')).toBeVisible();
-       await expect(page.locator('iframe[title="Spotify Embed\\: Al Sabaê Al Mounjiate \\(Quran\\)"]').contentFrame().getByRole('link', { name: 'Al Sabaê Al Mounjiate (Quran)' })).toBeVisible();
-       await expect(page.locator('iframe[title="Spotify Embed\\: Al Sabaê Al Mounjiate \\(Quran\\)"]').contentFrame().getByRole('link', { name: 'Abd Al Rahman Al Soudaiss' })).toBeVisible();
-       await expect(page.locator('iframe[title="Spotify Embed\\: Al Sabaê Al Mounjiate \\(Quran\\)"]').contentFrame().getByTestId('spotify-logo')).toBeVisible();
-       await expect(page.locator('iframe[title="Spotify Embed\\: Al Sabaê Al Mounjiate \\(Quran\\)"]').contentFrame().getByLabel('More')).toBeVisible();
-       await expect(page.locator('iframe[title="Spotify Embed\\: Al Sabaê Al Mounjiate \\(Quran\\)"]').contentFrame().getByTestId('play-pause-button')).toBeVisible();
-       await expect(page.locator('iframe[title="Spotify Embed\\: Al Sabaê Al Mounjiate \\(Quran\\)"]').contentFrame().getByTestId('tracklist-row-0')).toBeVisible();
-       await page.locator('iframe[title="Spotify Embed\\: Al Sabaê Al Mounjiate \\(Quran\\)"]').contentFrame().getByTestId('play-pause-button').click();
-       await page.waitForTimeout(300)
-       await page.locator('iframe[title="Spotify Embed\\: Al Sabaê Al Mounjiate \\(Quran\\)"]').contentFrame().getByTestId('tracklist-row-0').getByTestId('playback-indicator').click();
+        const pageLocator = page.locator('iframe[title="Spotify Embed\\: Al Sabaê Al Mounjiate \\(Quran\\)"]');
+
+        await expect(pageLocator.contentFrame().locator('.CoverArtBase_coverArt__ne0XI')).toBeVisible();
+        await expect(pageLocator.contentFrame().getByRole('link', { name: 'Al Sabaê Al Mounjiate (Quran)' })).toBeVisible();
+        await expect(pageLocator.contentFrame().getByRole('link', { name: 'Abd Al Rahman Al Soudaiss' })).toBeVisible();
+        await expect(pageLocator.contentFrame().getByTestId('spotify-logo')).toBeVisible();
+        await expect(pageLocator.contentFrame().getByLabel('More')).toBeVisible();
+        await expect(pageLocator.contentFrame().getByTestId('play-pause-button')).toBeVisible();
+        await expect(pageLocator.contentFrame().getByTestId('tracklist-row-0')).toBeVisible();
+        await pageLocator.contentFrame().getByTestId('play-pause-button').click();
+        await page.waitForTimeout(300)
+        await pageLocator.contentFrame().getByTestId('tracklist-row-0').getByTestId('playback-indicator').click();
     });
 
 });
