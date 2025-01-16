27d26e352e654ef134d4c7fecb0978acd6681f2a
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2025-01-16 08:54:50 +0600

    Classic Spotify Artist

--- tests/classic/spotify.spec.js
+++ tests/classic/spotify.spec.js
@@ -39,4 +39,20 @@ test.describe('Classic Spotify', () => {
         await page.waitForTimeout(300)
         await page.locator('iframe[title="Spotify Embed\\: Surah Ar-Rahman \\(Be Heaven\\)"]').contentFrame().getByTestId('play-pause-button').click();
     });
+
+    test('Classic Spotify Artist', async ({ page }) => {
+        const heading = page.getByText('Spotify Artist');
+        await heading.scrollIntoViewIfNeeded();
+        await expect(heading).toBeVisible();
+
+        await expect(page.locator('iframe[title="Spotify Embed\\: Omar Hisham"]').contentFrame().locator('.CoverArtBase_coverArt__ne0XI')).toBeVisible();
+        await expect(page.locator('iframe[title="Spotify Embed\\: Omar Hisham"]').contentFrame().getByTestId('spotify-logo')).toBeVisible();
+        await expect(page.locator('iframe[title="Spotify Embed\\: Omar Hisham"]').contentFrame().getByLabel('More')).toBeVisible();
+        await expect(page.locator('iframe[title="Spotify Embed\\: Omar Hisham"]').contentFrame().getByTestId('play-pause-button')).toBeVisible();
+        await expect(page.locator('iframe[title="Spotify Embed\\: Omar Hisham"]').contentFrame().getByTestId('tracklist-row-0')).toBeVisible();
+        await expect(page.locator('iframe[title="Spotify Embed\\: Omar Hisham"]').contentFrame().getByTestId('tracklist-row-1')).toBeVisible();
+        await page.locator('iframe[title="Spotify Embed\\: Omar Hisham"]').contentFrame().getByTestId('play-pause-button').click();
+        await page.waitForTimeout(300)
+        await page.locator('iframe[title="Spotify Embed\\: Omar Hisham"]').contentFrame().getByTestId('play-pause-button').click();
+    });
 });
