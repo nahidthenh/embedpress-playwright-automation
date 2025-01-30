cd8725207954769eb2f91a4435306b2541fa920c
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2025-01-30 12:45:21 +0600

    EmbedPress Auth Setup

---
+++ tests/auth.setup.js
@@ -0,0 +1,20 @@
+const { test: setup, expect } = require('@playwright/test');
+const path = require('path');
+
+const authFile = path.join(__dirname, '../playwright/.auth/user.json');
+
+const username = `${process.env.USERNAME}`;
+const password = `${process.env.PASSWORD}`;
+
+setup('authenticate', async ({ page }) => {
+    // Perform authentication steps. Replace these actions with your own.
+    await page.goto('https://embedpress.wpqa.site/wp-login.php');
+    await page.getByLabel('Username or Email Address').fill(username);
+    await page.getByLabel('Password', { exact: true }).fill(password);
+    await page.getByRole('button', { name: 'Log In' }).click();
+    await page.waitForURL('https://embedpress.wpqa.site/wp-admin/');
+    await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
+
+    // End of authentication steps.
+    await page.context().storageState({ path: authFile });
+});
