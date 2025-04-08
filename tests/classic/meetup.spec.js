32fd06b59f9aad69ff8586e5fca608d4b455ae6b
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2025-04-08 10:37:24 +0600

    WordWall Added Update MeetUp

--- tests/classic/meetup.spec.js
+++ tests/classic/meetup.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-test('Embed Gutenberg Gumroad source', async ({ page }) => {
+test('Embed Classic MeetUp source', async ({ page }) => {
     await page.goto('https://embedpress.wpqa.site/classic-meetup/');
     await expect(page.getByRole('link', { name: 'Scrum Master product Owner' })).toBeVisible();
     await expect(page.getByRole('link', { name: 'Hosted By Dhirender Verma S.' })).toBeVisible();
@@ -8,5 +8,5 @@ test('Embed Gutenberg Gumroad source', async ({ page }) => {
     await expect(page.getByRole('heading', { name: 'Details' })).toBeVisible();
 });
 
-// As now we only have embed support for Gumroad
+// As now we only have embed support for MeetUp
 
--- tests/gutenberg/meetup.spec.js
+++ tests/gutenberg/meetup.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-test('Embed Gutenberg Gumroad source', async ({ page }) => {
+test('Embed Gutenberg MeetUp source', async ({ page }) => {
     await page.goto('https://embedpress.wpqa.site/playwright-gutenberg/gutenberg-meetup/');
     await expect(page.getByRole('link', { name: 'Scrum Master product Owner' })).toBeVisible();
     await expect(page.locator('#ep-gutenberg-content-64fb49f3414eada2755e818947255be7').getByRole('article').locator('div').filter({ hasText: 'Hosted ByDhirender Verma S.' }).first()).toBeVisible();
@@ -8,5 +8,5 @@ test('Embed Gutenberg Gumroad source', async ({ page }) => {
     await expect(page.locator('div').filter({ hasText: /^Details$/ })).toBeVisible();
 });
 
-// As now we only have embed support for Gumroad
+// As now we only have embed support for MeetUp
 
