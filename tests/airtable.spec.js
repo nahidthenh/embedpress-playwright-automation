2a3987e42dbe72d70e1792c2ce65389aa640baf7
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2025-04-21 18:35:03 +0600

    Get back old testcase to tests folder

--- tests/airtable.spec.js
+++
@@ -1,8 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('Embed Classic Airtable source', async ({ page }) => {
-    await page.goto('https://embedpress.wpqa.site/playwright-classic-editor/classic-airtable/');
-    await expect(page.locator('iframe').contentFrame().locator('.dataRightPane')).toBeVisible();
-});
-// Update Test Case For Check cicd :1,
-// As now we only have embed support for Airtable
