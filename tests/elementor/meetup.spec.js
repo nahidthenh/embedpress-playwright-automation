8645e9f2a837eae78ca097157d8eebae602dcb51
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2025-05-04 10:22:40 +0600

    Fixed MeetUp Classic & Elementor Issue

--- tests/elementor/meetup.spec.js
+++ tests/elementor/meetup.spec.js
@@ -2,9 +2,9 @@ const { test, expect } = require('@playwright/test');
 
 test('Embed Elementor MeetUp source', async ({ page }) => {
     await page.goto('https://embedpress.wpqa.site/playwright-elementor/elementor-meetup/');
-    await expect(page.getByRole('link', { name: 'Scrum Master product Owner' })).toBeVisible();
-    await expect(page.getByRole('link', { name: 'Hosted By Dhirender Verma S.' })).toBeVisible();
-    await expect(page.getByRole('img', { name: 'Scrum Master product Owner' })).toBeVisible();
+    await page.goto('https://embedpress.wpqa.site/classic-meetup/');
+    await expect(page.getByRole('link', { name: 'MasterClass: BrandBooster -' })).toBeVisible();
+    await page.getByRole('img', { name: 'MasterClass: BrandBooster -' }).click();
     await expect(page.getByRole('heading', { name: 'Details' })).toBeVisible();
 });
 
