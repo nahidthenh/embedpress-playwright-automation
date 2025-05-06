b40e3a8a57e8c29ea4e86b6565f859d494b34ea3
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2025-05-06 12:08:53 +0600

    GU - Google Photos Grid Layout

---
+++ tests/gutenberg/gu-google-photos-grid-layout.spec.js
@@ -0,0 +1,16 @@
+const { test, expect } = require('@playwright/test');
+
+
+test('Visibility of Google Photos Grid Layout in Gutenberg', async ({ page }) => {
+
+    await page.goto('https://embedpress.wpqa.site/gu-google-photos-grid-layout/');
+
+    await expect(page.getByRole('heading', { name: 'Gu Google Photos – Grid Layout' })).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Startise Pitha Utshob 5.0 –' })).toBeVisible();
+    await expect(page.locator('#photo-c4c389e29d69c63787aba1e1fb448a5d > img')).toBeVisible();
+    await expect(page.locator('#photo-fe3a9f5230123699055972327c853a39').getByRole('img', { name: 'Photo' })).toBeVisible();
+    await page.locator('#photo-fe3a9f5230123699055972327c853a39').getByRole('img', { name: 'Photo' }).click();
+    await expect(page.locator('#close-btn').getByRole('img')).toBeVisible();
+    await expect(page.locator('#next-btn')).toBeVisible();
+    await expect(page.locator('#prev-btn')).toBeVisible();
+});
