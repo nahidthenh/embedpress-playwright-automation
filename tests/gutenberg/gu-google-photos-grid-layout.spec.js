6a9c967613036ddc8b289fa456ba41cb3e9de5b9
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2025-05-06 12:24:01 +0600

    Added TestCase for - GU Google Photos Masonry Layout

--- tests/gutenberg/gu-google-photos-grid-layout.spec.js
+++ tests/gutenberg/gu-google-photos-grid-layout.spec.js
@@ -6,6 +6,11 @@ test('Visibility of Google Photos Grid Layout in Gutenberg', async ({ page }) =>
     await page.goto('https://embedpress.wpqa.site/gu-google-photos-grid-layout/');
 
     await expect(page.getByRole('heading', { name: 'Gu Google Photos – Grid Layout' })).toBeVisible();
+
+    // Grid layout visibility 
+    const masonry = page.locator('.google-photos-gallery-grid-widget');
+    await expect(masonry).toBeVisible();
+
     await expect(page.getByRole('heading', { name: 'Startise Pitha Utshob 5.0 –' })).toBeVisible();
     await expect(page.locator('#photo-c4c389e29d69c63787aba1e1fb448a5d > img')).toBeVisible();
     await expect(page.locator('#photo-fe3a9f5230123699055972327c853a39').getByRole('img', { name: 'Photo' })).toBeVisible();
---
+++ tests/gutenberg/gu-google-photos-masonry-layout.spec.js
@@ -0,0 +1,20 @@
+const { test, expect } = require('@playwright/test');
+
+
+test('Visibility of Google Photos Masonry Layout in Gutenberg', async ({ page }) => {
+
+    await page.goto('https://embedpress.wpqa.site/playwright-gutenberg/gu-google-photos-masonry-layout/');
+
+    await expect(page.getByRole('heading', { name: 'GU Google Photos Masonry' })).toBeVisible();
+
+    // masonry layout visibility 
+    const masonry = page.locator('.google-photos-gallery-masonary-widget');
+    await expect(masonry).toBeVisible();
+
+
+    await expect(page.getByRole('heading', { name: 'Startise Pitha Utshob 5.0 –' })).toBeVisible();
+    await page.locator('#photo-c4c389e29d69c63787aba1e1fb448a5d').getByRole('img', { name: 'Photo' }).click();
+    await expect(page.locator('#close-btn').getByRole('img')).toBeVisible();
+    await expect(page.locator('#next-btn')).toBeVisible();
+    await expect(page.locator('#prev-btn')).toBeVisible();
+});
