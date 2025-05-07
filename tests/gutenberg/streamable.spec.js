63bb56357ee88b69ffeea8415ef2855597cfbc6b
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2025-05-07 11:08:02 +0600

    Added Streamble TestCases for all editors

---
+++ tests/gutenberg/streamable.spec.js
@@ -0,0 +1,8 @@
+const { test, expect } = require('@playwright/test');
+
+test('Embed Gutenberg Streamable source', async ({ page }) => {
+    await page.goto('https://embedpress.wpqa.site/gu-streamable/');
+    await expect(page.locator('iframe[title="I am Napoleon\\. I am Emperor x Rammstein - Sonne \\(slowed\\) - jez \\(144p\\, h264\\)"]').contentFrame().locator('div').filter({ hasText: /^Watch againStream will resume when your device's connection improves$/ }).first()).toBeVisible();
+});
+
+// As now we only have embed support 
