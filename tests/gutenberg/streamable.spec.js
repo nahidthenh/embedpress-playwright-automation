2fd520c1a51878ce55fc0479e12ed9d8d2e29373
Karima <skarima.faiza@gmail.com>
2025-05-08 13:03:23 +0600

    commit el_ted

--- tests/gutenberg/streamable.spec.js
+++ tests/gutenberg/streamable.spec.js
@@ -5,4 +5,4 @@ test('Embed Gutenberg Streamable source', async ({ page }) => {
     await expect(page.locator('iframe[title="I am Napoleon\\. I am Emperor x Rammstein - Sonne \\(slowed\\) - jez \\(144p\\, h264\\)"]').contentFrame().locator('div').filter({ hasText: /^Watch againStream will resume when your device's connection improves$/ }).first()).toBeVisible();
 });
 
-// As now we only have embed support 
+// As now we only have embed support
