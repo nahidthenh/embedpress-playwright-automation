ab2067ce7a8bb058d495eea169458a343c52fed6
Karima <skarima.faiza@gmail.com>
2025-05-08 17:24:00 +0600

    Codepoints, Datawrapper

---
+++ tests/gutenberg/codepoints.spec.js
@@ -0,0 +1,8 @@
+const { test, expect } = require('@playwright/test');
+
+test('Embed Gutenberg Codpoints source', async ({ page }) => {
+    await page.goto('https://embedpress.wpqa.site/gu-codepoints/');await expect(page.locator('iframe[title="U\\+0001 START OF HEADING\\*"]').contentFrame().getByRole('img', { name: 'Glyph for U+' })).toBeVisible();
+    await expect(page.locator('.ep-embed-content-wraper')).toBeVisible();
+});
+
+// As now we only have embed support 
