78cd057df68019b84233733e040d1d54b06ff488
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2025-06-18 11:09:52 +0600

    Add Playwright test for embedding Classic SAPO source

---
+++ tests/classic/sapo.spec.js
@@ -0,0 +1,7 @@
+
+const { test, expect } = require('@playwright/test');
+
+test('Embed Classic SAPO source', async ({ page }) => {
+    await page.goto('');
+});
+// As now we only have embed support for Rumble
