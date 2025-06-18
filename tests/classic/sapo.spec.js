dba75e8a180206861a92e93b93af35986cedfc8f
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2025-06-18 11:10:11 +0600

    Remove Playwright test for embedding Classic SAPO source

--- tests/classic/sapo.spec.js
+++
@@ -1,7 +0,0 @@
-
-const { test, expect } = require('@playwright/test');
-
-test('Embed Classic SAPO source', async ({ page }) => {
-    await page.goto('');
-});
-// As now we only have embed support for Rumble
