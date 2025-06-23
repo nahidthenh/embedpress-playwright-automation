34e446a046569ef9cd555e74c141c8bb77397707
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2025-06-23 15:08:10 +0600

    Refactor Playwright tests for Gutenberg Modern PDF: streamline test cases and enhance control visibility checks

--- tests/gutenberg/modern_pdf.spec.js
+++ tests/gutenberg/modern_pdf.spec.js
@@ -5,143 +5,34 @@ let heading = 'Gutenberg Modern pdf';
 
 
 test.describe("Gutenberg Modern PDF", () => {
-    if (process.env.CI) {
-        test.skip('Skipping all tests in this describe block in CI');
-    }
-    test.beforeEach(async ({ page }) => {
+
+    // PDF Blocks Embed
+    test('PDF Blocks Embed', async ({ page }) => {
         await page.goto(slug);
-        await page.waitForLoadState('networkidle');
-        await expect.soft(page.getByRole('heading', { name: heading, exact: true })).toBeVisible();
-    });
+        await expect(page.getByRole('heading', { name: heading })).toBeVisible();
 
-    test('To Enable All Controls', async ({ page }) => {
-        const frameSelector = '#embedpress-pdf-1721988006079 iframe[title="sample_pdf"]';
-        const mainFrame = page.frameLocator(frameSelector);
+        const iframe = page.locator('#embedpress-pdf-1721988006079 iframe[title="sample_pdf"]');
+        const frame = iframe.contentFrame();
 
-        const controls = [
-            { role: 'button', name: 'Zoom In' },
-            { role: 'button', name: 'Zoom Out' },
-            { role: 'button', name: 'Presentation Mode' },
-            { role: 'button', name: 'Print' },
-            { role: 'button', name: 'Save' },
-            { role: 'button', name: 'Toggle Sidebar' },
-            { role: 'button', name: 'Find' },
-            { role: 'button', name: 'Next' },
-            { role: 'spinbutton', name: 'Page' },
-        ];
-
-        // Check visibility of all controls
-        for (const control of controls) {
-            await expect(mainFrame.getByRole(control.role, { name: control.name })).toBeVisible();
-        }
-
-        // Perform additional actions
-        await mainFrame.getByRole('button', { name: 'Tools' }).click();
-
-        const extraElements = [
-            { selector: '#scaleSelect' },
-            { text: 'of ⁨4⁩' },
-            { selector: '#secondaryPrint' },
-            { selector: '#secondaryDownload' },
-        ];
-
-        // Check visibility of extra elements
-        for (const element of extraElements) {
-            if (element.selector) {
-                await expect(mainFrame.locator(element.selector)).toBeVisible();
-            } else if (element.text) {
-                await expect(mainFrame.getByText(element.text)).toBeVisible();
-            }
-        }
-
-        const toolsControls = [
-            { role: 'button', name: 'Tools' },
-            { role: 'link', name: 'Current Page' },
-            { role: 'button', name: 'Go to Last Page' },
-            { role: 'button', name: 'Rotate Clockwise' },
-            { role: 'button', name: 'Rotate Counterclockwise' },
-            { role: 'radio', name: 'Text Selection Tool' },
-            { role: 'radio', name: 'Hand Tool' },
-            { role: 'radio', name: 'Page Scrolling' },
-            { role: 'radio', name: 'Vertical Scrolling' },
-            { role: 'radio', name: 'Horizontal Scrolling' },
-            { role: 'radio', name: 'Wrapped Scrolling' },
-            { role: 'radio', name: 'No Spreads' },
-            { role: 'radio', name: 'Odd Spreads' },
-            { role: 'radio', name: 'Even Spreads' },
-            { role: 'button', name: 'Document Properties…' },
-        ];
-        // Check visibility of all controls
-        for (const toolsTontrol of toolsControls) {
-            await expect(mainFrame.getByRole(toolsTontrol.role, { name: toolsTontrol.name })).toBeVisible();
-        }
-
-        await mainFrame.getByRole('button', { name: 'Document Properties…' }).click();
-        await expect(page.frameLocator('iframe[title="sample_pdf"]').nth(0).getByText('File name: sample_pdf.pdf File size: ⁨86.2⁩ KB (⁨88,226⁩ bytes) Title: PDF')).toBeVisible();
+        await expect(page.getByRole('heading', { name: 'PDF Blocks Embed' })).toBeVisible();
+        await expect(frame.getByRole('button', { name: 'Presentation Mode' })).toBeVisible();
+        await expect(frame.getByRole('button', { name: 'Save' })).toBeVisible();
+        await expect(frame.getByRole('button', { name: 'Print' })).toBeVisible();
     });
 
 
+    // Enable Pro Controls PDF Blocks
+    test('Enable Pro Controls PDF Blocks', async ({ page }) => {
+        await page.goto(slug);
+        await expect(page.getByRole('heading', { name: 'Enable Pro Controls PDF Blocks' })).toBeVisible();
 
-    test('To Enable Few Controls', async ({ page }) => {
-        const frameSelector = '#some_control_disable iframe[title="sample_pdf"]';
-        const mainFrame = page.frameLocator(frameSelector);
+        const iframe = page.locator('#embedpress-pdf-1721994414562 iframe[title="sample_pdf"]');
+        const frame = iframe.contentFrame();
 
-        const controls = [
-            { role: 'radio', name: 'Text' },
-            { role: 'radio', name: 'Draw' },
-            { role: 'radio', name: 'Add or edit images' },
-            { role: 'button', name: 'Zoom In' },
-            { role: 'button', name: 'Zoom Out' },
-            { role: 'button', name: 'Toggle Sidebar' },
-            { role: 'button', name: 'Find' },
-            { role: 'button', name: 'Next' },
-            { role: 'spinbutton', name: 'Page' },
-            { role: 'button', name: 'Tools' }
-        ];
-
-        const extraElements = [
-            { selector: '#scaleSelect' },
-            { text: 'of ⁨4⁩' },
-        ];
-
-        // Check visibility of all controls
-        for (const control of controls) {
-            await expect(mainFrame.getByRole(control.role, { name: control.name })).toBeVisible();
-        }
-
-        // Check visibility of extra elements
-        for (const element of extraElements) {
-            if (element.selector) {
-                await expect(mainFrame.locator(element.selector)).toBeVisible();
-            } else if (element.text) {
-                await expect(mainFrame.getByText(element.text)).toBeVisible();
-            }
-        }
-
-        // Perform additional actions
-        await mainFrame.getByRole('button', { name: 'Tools' }).click();
-
-
-        const toolsControls = [
-            { role: 'link', name: 'Current Page' },
-            { role: 'button', name: 'Go to Last Page' },
-            { role: 'button', name: 'Rotate Clockwise' },
-            { role: 'button', name: 'Rotate Counterclockwise' },
-            { role: 'radio', name: 'Text Selection Tool' },
-            { role: 'radio', name: 'Hand Tool' },
-            { role: 'radio', name: 'Page Scrolling' },
-            { role: 'radio', name: 'Vertical Scrolling' },
-            { role: 'radio', name: 'Horizontal Scrolling' },
-            { role: 'radio', name: 'Wrapped Scrolling' },
-            { role: 'radio', name: 'No Spreads' },
-            { role: 'radio', name: 'Odd Spreads' },
-            { role: 'radio', name: 'Even Spreads' },
-        ];
-        // Check visibility of all controls
-        for (const toolControl of toolsControls) {
-            await expect(mainFrame.getByRole(toolControl.role, { name: toolControl.name })).toBeVisible();
-        }
-
+        // Check Pro Feature Download/Print Should not visibile 
+        await expect(frame.getByRole('button', { name: 'Presentation Mode' })).toBeVisible();
+        await expect(frame.getByRole('button', { name: 'Save' })).toBeHidden();
+        await expect(frame.getByRole('button', { name: 'Print' })).toBeHidden();
     });
 
 });
