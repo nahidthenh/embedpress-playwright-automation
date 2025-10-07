754cee6900750f30ddcca02290d5580f6aab44bb
Md. Nahid Hasan <42797913+nahidthenh@users.noreply.github.com>
2025-10-07 09:25:17 +0600

    Fix: Update Meetup test to check for correct elements visibility

--- tests/classic/meetup.spec.js
+++ tests/classic/meetup.spec.js
@@ -2,7 +2,9 @@ const { test, expect } = require('@playwright/test');
 
 test('Embed Classic MeetUp source', async ({ page }) => {
     await page.goto('https://ep-automation.wpqa.site/classic-meetup/');
-    await expect(page.getByRole('heading', { name: 'Details' })).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Newark Tech Week' })).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Events - Graph Database' })).toBeVisible();
+    await expect(page.locator('.ep-single-event > .ep-event-content').first()).toBeVisible();
 });
 
 // As now we only have embed support for MeetUp
